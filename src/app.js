bdeca7b9f9e5d40aafee157f76f0a199fbd61777
EScott237 <143569907+EScott237@users.noreply.github.com>
2023-09-17 06:24:12 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#the-excuse").innerHTML = generateExcuse();
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let article = ["A", "The"];
+  let subject = ["burglar", "cat", "dog", "monkey", "donkey"];
+  let action = ["stole my", "slapped my", "punched my", "threw my"];
+  let possession = ["homework", "keys", "laptop", "phone"];
+  let where = ["at my house", "in my car", "on the way", "at the park"];
+
+  let articleIndex = Math.floor(Math.random() * article.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possessionIndex = Math.floor(Math.random() * possession.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return [
+    article[articleIndex] +
+      " " +
+      subject[subjectIndex] +
+      " " +
+      action[actionIndex] +
+      " " +
+      possession[possessionIndex] +
+      " " +
+      where[whereIndex]
+  ];
 };
